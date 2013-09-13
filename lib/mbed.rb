c4c7f56abdd1adc6420f3ade6fe62d41f87a2737
Arthur Reutenauer <Arthur.Reutenauer@xively.com>
2013-09-13 13:18:23 +0100

    Making it work!

--- lib/mbed.rb
+++ lib/mbed.rb
@@ -1,4 +1,15 @@
+require 'yaml'
+require 'net/http'
+require 'json'
+
 class Mbed
   def self.read
+    credentials = YAML.load(File.read(File.expand_path('../../config/credentials.yml', __FILE__)))
+    id = credentials['id']
+    key = credentials['key']
+    # TODO HTTPS (Net::HTTP protests about certificate)
+    uri = URI("http://api.xively.com/v2/feeds/#{id}?key=#{key}")
+    resp = Net::HTTP.get_response(uri)
+    JSON.parse(resp.body)
   end
 end
--- spec/mbed_spec.rb
+++ spec/mbed_spec.rb
@@ -5,5 +5,14 @@ describe Mbed do
     it "doesn’t crash" do
       Mbed.read
     end
+
+    it "returns a hash" do
+      Mbed.read.should be_a Hash
+    end
+
+    it "does contains “Unauthorized” in the hash" do
+      response = Mbed.read
+      response['title'].should_not == "Unauthorized"
+    end
   end
 end
