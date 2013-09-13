d6bb7e548749354f29ff9acecdb0fdf824217cae
Arthur Reutenauer <Arthur.Reutenauer@xively.com>
2013-09-13 13:54:42 +0100

    Added output of “rails new”.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fri13::Application.config.secret_key_base = 'd927b8dd856712a9f4f70c6fb50cf3bf6c44f5f27f9046eccae7a7f8aed66a94f75ad72d230e86978347f7293f7a1111e4bca7200939bd16ea690abc69cd3868'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fri13::Application.config.session_store :cookie_store, key: '_fri13_session'
