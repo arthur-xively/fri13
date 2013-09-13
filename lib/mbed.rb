3f44036109f8ba3863d4794de5b89d03bd53580d
Arthur Reutenauer <Arthur.Reutenauer@xively.com>
2013-09-13 14:31:08 +0100

    Done with my changes.

--- lib/mbed.rb
+++ lib/mbed.rb
@@ -3,12 +3,14 @@ require 'net/http'
 require 'json'
 
 class Mbed
+  attr_reader :id, :key
+
   def read
     credentials = YAML.load(File.read(File.expand_path('../../config/credentials.yml', __FILE__)))
-    id = credentials['id']
-    key = credentials['key']
+    @id = credentials['id']
+    @key = credentials['key']
     # TODO HTTPS (Net::HTTP protests about certificate)
-    uri = URI("http://api.xively.com/v2/feeds/#{id}?key=#{key}")
+    uri = URI("http://api.xively.com/v2/feeds/#{@id}?key=#{@key}")
     resp = Net::HTTP.get_response(uri)
     raise unless resp.code == '200' # Could be 403
     feed = JSON.parse(resp.body)
